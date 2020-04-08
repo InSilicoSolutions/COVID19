3de37535d635a5bb6bff8f5322cc0bdd5a80dc01
Michael Ryan <mryan@insilico.us.com>
2020-04-08 16:22:57 -0400

    Updates to protein viewer for position and different styles

--- embed.js
+++ embed.js
@@ -13,7 +13,8 @@ var opts = {
     zoomer: { 
         menuFontsize: "12px", 
         autoResize: true, 
-        alignmentHeight: 700, 
+        alignmentHeight: 700,
+        alignmentWidth: "auto",
         labelNameLength: 180,
     },
 };
@@ -34,4 +35,30 @@ schemer.set('scheme','altered');
 var menuOpts = {el: document.getElementById('div'), msa: m};
 var defMenu = new msa.menu.defaultmenu(menuOpts);
 m.addView("menu", defMenu);
+reSize();
 m.render();
+
+function injectOption(){
+	var optionName = "Differences"
+	var styleDropdown = document.getElementsByClassName("smenu-dropdown-menu")[5];
+	firstOption = styleDropdown.childNodes[0]
+	if (firstOption.textContent != optionName){
+		var newOption = document.createElement("LI"); 
+		newOption.style = "line-height: 14px;";
+		newOption.textContent = optionName;
+		newOption.onclick = styleSwitch
+		styleDropdown.insertBefore(newOption, firstOption);
+	}
+	
+}
+
+function styleSwitch(){
+	schemer.set('scheme','altered');
+}
+
+function reSize(){
+	m.g.zoomer.set("alignmentWidth", window.innerWidth-220)
+	m.g.zoomer.set("alignmentHeight", window.innerHeight-183)
+}
+document.onclick = injectOption
+window.addEventListener("resize", reSize, false);
