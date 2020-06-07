5e34cd122ff71bee7a2c23ac15b5beec97888823
kmoad <kyle.moad@gmail.com>
2020-06-07 19:04:18 -0400

    tour function

--- viewer/main.js
+++ viewer/main.js
@@ -6,11 +6,45 @@ VVGTFKAKDLIVTPATILKEKPDPNNLVFGTVFTDHMLTVEWSSEFGWEKPHIKPLQNLSLHPGSSALHYAVELFEGLKA
   Score=358
 `.trim();
 var alignId;
+
 window.onload = () => {
-    const div = document.querySelector('#viewer');
-    v = new proteinWidget.ProteinViewer(div);
-    v.loadPDB('2ABJ').then(()=>{
-        alignId = v.addAlignment('G',align);
-        v.highlight(alignId,165,200);
-    });
+  const div = document.querySelector('#viewer');
+  v = new proteinWidget.ProteinViewer(div);
+  v.loadPDB('2ABJ').then(()=>{
+    alignId = v.addAlignment('G',align);
+    v.highlight(alignId,165,200);
+  });
+  
+}
+
+const tour = () => {
+  const sleep = ms => new Promise( r => setTimeout(r, ms));
+  (async () => {
+    console.log('Load protein');
+    await v.loadPDB('2ABJ')
+    await sleep(5000);
+    console.log('Add alignment and highlight segment');
+    alignId = v.addAlignment('G',align);
+    v.highlight(alignId,165,200);
+    await sleep(5000);
+    console.log('Draw surface');
+    v.toggleSurface();
+    await sleep(5000);
+    console.log('Remove highlight');
+    v.unHighlight();
+    await sleep(5000);
+    console.log('Remove surface');
+    v.toggleSurface();
+    await sleep(5000);
+    console.log('Switch to sphere');
+    v.presetStyle('sphere');
+    await sleep(5000);
+    console.log('Draw surface again');
+    v.toggleSurface();
+    await sleep(5000);
+    console.log('Highlight a different portion');
+    v.highlight(alignId,10,200);
+
+  })()
+
 }
